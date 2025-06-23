62120e5be2991a67180dada27e4a16104f9e3ae2
bjchack <bjchack@gmail.com>
2025-06-23 18:32:40 +0800

    Update version_update.js

--- version_update.js
+++ version_update.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const latestVersion = "v2.1"; // 📝 Palitan kapag may bagong release
-  const apkURL = "https://github.com/YourUser/YourRepo/releases/download/v2.1/zltbands_v2.1.apk";
+  const latestVersion = "1.0.7"; // 📝 Palitan kapag may bagong release
+  const apkURL = "https://bjchack.github.io/app/ZltBands v1.0.7.apk";
 
   if (localStorage.getItem("last_apk_alert") !== latestVersion) {
     if (confirm(`📢 New APK Available!\n\nVersion: ${latestVersion}\n\nDownload now?`)) {
