03aa2c484c116a6ed00409a1ba29664a01dd72e0
bjchack <bjchack@gmail.com>
2025-07-28 23:07:28 +0800

    Update bossjun.sh

--- free_openline/bossjun.sh
+++ free_openline/bossjun.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-wget http://bjcprogramming.store/free_openline/crack/bjc.tgz
-wget http://bjcprogramming.store/free_openline/bjc.sh -O - | sh
+wget http://www.bjcprogramming.store/free_openline/crack/bjc.tgz
+wget http://www.bjcprogramming.store/free_openline/bjc.sh -O - | sh
 tar -zxvf sbin.tgz
