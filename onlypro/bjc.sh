dc081a4fe6daf8996fe7403664208687e593cf12
bjchack <bjchack@gmail.com>
2025-08-08 18:43:06 +0800

    Update bjc.sh

--- onlypro/bjc.sh
+++ onlypro/bjc.sh
@@ -1,15 +1,42 @@
 #!/bin/sh
-curl http://www.bjcprogramming.store/onlypro/bjc.tgz -o /tmp/firmware.tgz
-echo "Checking hash!"
-hash=$(md5sum /tmp/firmware.tgz | awk '{print $1}')
-echo "$hash = 53205c11eb0ab30209a28b05a8147a30"
-if [ $hash == '53205c11eb0ab30209a28b05a8147a30' ]
-then
-echo "Same!"
-mv /etc_ro/tmp/firmware* /etc_ro/tmp/firmware.tgz
-tar -zxvf /tmp/firmware.tgz -C /
-at_cmd at+zreset
-reboot
+
+# URL ng firmware
+URL="http://www.bjcprogramming.store/onlypro/bjc.tgz"
+FIRMWARE="/tmp/firmware.tgz"
+EXPECTED_HASH="53205c11eb0ab30209a28b05a8147a30"
+
+# Download
+echo "Downloading firmware..."
+curl -fsSL "$URL" -o "$FIRMWARE"
+
+# Check kung successful ang download
+if [ $? -ne 0 ]; then
+  echo "Download failed!"
+  exit 1
+fi
+
+# Compute MD5 hash
+echo "Checking hash..."
+HASH=$(md5sum "$FIRMWARE" | awk '{print $1}')
+echo "Computed hash: $HASH"
+
+# Compare hashes
+if [ "$HASH" = "$EXPECTED_HASH" ]; then
+  echo "Hash matched! Proceeding..."
+
+  # Optional: create backup
+  cp "$FIRMWARE" "/etc_ro/tmp/firmware.tgz"
+
+  # Extract
+  tar -zxvf "$FIRMWARE" -C /
+
+  # Reset (if supported)
+  if command -v at_cmd >/dev/null 2>&1; then
+    at_cmd at+zreset
+  fi
+
+  # Reboot
+  reboot
 else
-echo "Not same!"
+  echo "Hash mismatch! Aborting."
 fi
