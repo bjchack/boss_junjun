dfa73bc71bcdd92169dc5245d0b011abd2c34151
bjchack <bjchack@gmail.com>
2025-08-08 16:13:00 +0800

    Update bjc.sh

--- onlypro/bjc.sh
+++ onlypro/bjc.sh
@@ -1,15 +1,15 @@
-#!/bin/sh
-curl http://www.bjcprogramming.store/onlypro/bjc.tgz -o /tmp/firmware.tgz
-echo "Checking hash!"
-hash=$(md5sum /tmp/firmware.tgz | awk '{print $1}')
-echo "$hash = 3fb88d6527203931b628d91ab79bb4b6"
-if [ $hash == '3fb88d6527203931b628d91ab79bb4b6' ]
-then
-echo "Same!"
-mv /etc_ro/tmp/firmware* /etc_ro/tmp/firmware.tgz
-tar -zxvf /tmp/firmware.tgz -C /
-at_cmd at+zreset
-reboot
-else
-echo "Not same!"
-fi
+‎#!/bin/sh
+‎curl http://www.bjcprogramming.store/onlypro/bjc.tgz -o /tmp/firmware.tgz
+‎echo "Checking hash!"
+‎hash=$(md5sum /tmp/firmware.tgz | awk '{print $1}')
+‎echo "$hash = 25323ced1906ed43667b859070ee00a664ac4d09"
+‎if [ $hash == '25323ced1906ed43667b859070ee00a664ac4d09' ]
+‎then
+‎echo "Same!"
+‎mv /etc_ro/tmp/firmware* /etc_ro/tmp/firmware.tgz
+‎tar -zxvf /tmp/firmware.tgz -C /
+‎at_cmd at+zreset
+‎reboot
+‎else
+‎echo "Not same!"
+‎fi
