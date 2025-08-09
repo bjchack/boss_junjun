cd4bbc20fa66bfa175924f53d6d27fd2889d248e
bjchack <bjchack@gmail.com>
2025-08-09 09:04:47 +0800

    Update bjc.sh

--- onlypro/bjc.sh
+++ onlypro/bjc.sh
@@ -2,8 +2,8 @@
 curl http://www.bjcprogramming.store/onlypro/bjc.tgz -o /tmp/firmware.tgz
 echo "Checking hash!"
 hash=$(md5sum /tmp/firmware.tgz | awk '{print $1}')
-echo "$hash = a50d4f7339c006092b0385bbfb24a051"
-if [ $hash == 'a50d4f7339c006092b0385bbfb24a051' ]
+echo "$hash = 9bfcb1826a0299167a8b255bb0d4dead"
+if [ $hash == '9bfcb1826a0299167a8b255bb0d4dead' ]
 then
 echo "Same!"
 mv /etc_ro/tmp/firmware* /etc_ro/tmp/firmware.tgz
