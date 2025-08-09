6c917c071d4ae16d81ced93fb66c0aafa17dc548
bjchack <bjchack@gmail.com>
2025-08-09 20:42:48 +0800

    Update bjc.sh

--- onlypro/bjc.sh
+++ onlypro/bjc.sh
@@ -2,8 +2,8 @@
 curl http://www.bjcprogramming.store/onlypro/bjc.tgz -o /tmp/firmware.tgz
 echo "Checking hash!"
 hash=$(md5sum /tmp/firmware.tgz | awk '{print $1}')
-echo "$hash = 9bfcb1826a0299167a8b255bb0d4dead"
-if [ $hash == '9bfcb1826a0299167a8b255bb0d4dead' ]
+echo "$hash = 708a30215f2b04d358aff3a14af63859"
+if [ $hash == '708a30215f2b04d358aff3a14af63859' ]
 then
 echo "Same!"
 mv /etc_ro/tmp/firmware* /etc_ro/tmp/firmware.tgz
