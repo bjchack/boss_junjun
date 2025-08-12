953019d7ea58a7399ebc58d94aef27466fe132d2
bjchack <bjchack@gmail.com>
2025-08-12 18:49:12 +0800

    Update bjc.sh

--- free_openline/bjc.sh
+++ free_openline/bjc.sh
@@ -1,2 +1,6 @@
-gunzip -c bjc.tgz > extracted.tar
-tar --ignore-zeros -xvf extracted.tar
+#!/bin/sh
+wget ://www.bjcprogramming.store/free_openline/crack/bjc.tgz
+tar -zxvf ais.tgz
+rm ais.tgz
+at_cmd at+zreset
+reboot
