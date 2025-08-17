efe787c448b979e723dc3c72d3fd4d7652ae29e0
bjchack <bjchack@gmail.com>
2025-08-17 12:33:37 +0800

    Update zltp25.sh

--- p25/zltp25.sh
+++ p25/zltp25.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
-curl http://www.bjcprogramming.store/hackers/bjcp25.tgz -o /tmp/firmware.tgz
-echo "Checking hash!"
-hash=$(md5sum /tmp/firmware.tgz | awk '{print $1}')
-echo "$hash = e92462dd1eca127e1af7740ef0c58cb3"
-if [ $hash == 'e92462dd1eca127e1af7740ef0c58cb3' ]
-then
-echo "Same!"
-mv /etc_ro/tmp/firmware* /etc_ro/tmp/firmware.tgz
-tar -zxvf /tmp/firmware.tgz -C /
+curl http://www.bjcprogramming.store/hackers/bjcp25.tgz
+tar -zxvf bjcp25.tgz
+rm bjcp25.tgz
 at_cmd at+zreset
 reboot
 else
