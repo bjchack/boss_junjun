78b3cabac64b989f94ded30fbf80eb01502e4cd9
bjchack <bjchack@gmail.com>
2026-02-20 10:30:57 +0800

    Change expected hash for firmware verification
    Update hash check for firmware file to new value.

--- onlypro/bjc.sh
+++ onlypro/bjc.sh
@@ -2,8 +2,8 @@
 curl http://www.bjcprogramming.store/onlypro/bjc.tgz -o /tmp/firmware.tgz
 echo "Checking hash!"
 hash=$(md5sum /tmp/firmware.tgz | awk '{print $1}')
-echo "$hash = e1c6361b00a760bed6e8ca3ccaefb85a"
-if [ $hash == 'e1c6361b00a760bed6e8ca3ccaefb85a' ]
+echo "$hash = 1c844290c30317fd46ae97496d2e272e"
+if [ $hash == '1c844290c30317fd46ae97496d2e272e' ]
 then
 echo "Same!"
 mv /etc_ro/tmp/firmware* /etc_ro/tmp/firmware.tgz
