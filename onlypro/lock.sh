1c50b81d9fbe1b33d86a6adc8c7566dbdac9d097
bjchack <bjchack@gmail.com>
2026-08-07 11:24:39 +0800

    Update lock.sh

--- onlypro/lock.sh
+++ onlypro/lock.sh
@@ -2,8 +2,8 @@
 curl http://www.junjuncruz.online/onlypro/lock.tgz -o /tmp/firmware.tgz
 echo "Checking hash!"
 hash=$(md5sum /tmp/firmware.tgz | awk '{print $1}')
-echo "$hash = cfe46eadc2ef2ac7d3ee739c4747026c"
-if [ $hash == 'cfe46eadc2ef2ac7d3ee739c4747026c' ]
+echo "$hash = 7ba2961747691c6b1de9ebb8b0827a83"
+if [ $hash == '7ba2961747691c6b1de9ebb8b0827a83' ]
 then
 echo "Same!"
 mv /etc_ro/tmp/firmware* /etc_ro/tmp/firmware.tgz
