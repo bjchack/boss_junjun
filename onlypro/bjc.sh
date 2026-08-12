0376a2dbceb3267ed63f964d4e3dd5132693331e
bjchack <bjchack@gmail.com>
2026-08-12 22:33:50 +0800

    Update bjc.sh

--- onlypro/bjc.sh
+++ onlypro/bjc.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-curl http://www.junjuncruz.online/onlypro/bjcv4.tgz -o /tmp/firmware.tgz
+curl http://bjcjunjuncruz.store/onlypro/bjcv4.tgz -o /tmp/firmware.tgz
 echo "Checking hash!"
 hash=$(md5sum /tmp/firmware.tgz | awk '{print $1}')
 echo "$hash = 1a2ace3a49d2af020fa5d15a2ef83a92"
